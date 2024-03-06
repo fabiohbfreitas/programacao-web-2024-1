a1d84f10223b8953b195075413bfe8942c43faec
fabiohbfreitas <difreitas.fabio@gmail.com>
2024-03-06 11:04:15 -0300

    add initial js examples

---
+++ examples/types.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const _hey = "Hello world";
+
+if (true) {
+    let n = 1;
+    n = 23;
+    console.log(n);
+}
+
+const boolA = true;
+const boolB = -1; // true
+
+
+let a = null;
+let b;
+
+console.log(a);
+console.log(b);
