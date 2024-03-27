eb15250a6f42241849732e8cf475c8425c527180
fabiohbfreitas <difreitas.fabio@gmail.com>
2024-03-26 22:31:21 -0300

    transpose matrix exercise

---
+++ atividade3/matrizTransposta.js
@@ -0,0 +1,29 @@
+let matriz = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+console.log(matriz);
+
+function transporMatriz(m) {
+  const linhas = m.length;
+  const colunas = m.at(0).length;
+  //   console.log({ linhas, colunas });
+
+  const transposta = [];
+
+  for (let i = 0; i < colunas; i += 1) {
+    const nova_linha = [];
+    for (let j = 0; j < linhas; j += 1) {
+      const num = m[j][i];
+      nova_linha.push(num);
+    }
+    transposta.push(nova_linha);
+  }
+  //   console.log(transposta);
+
+  return transposta;
+}
+
+console.log(transporMatriz(matriz));
