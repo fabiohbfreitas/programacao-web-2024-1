cb89f5ab8bda36dbcb0af9bb1115389733a7fd07
fabiohbfreitas <difreitas.fabio@gmail.com>
2024-03-27 11:06:03 -0300

    formatting

--- modulos/index.js
+++ modulos/index.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 9091;
 
 app.get("/", (_req, res) => {
-    return res.send("Hello, world!");
-})
+  return res.send("Hello, world!");
+});
 
 app.listen(port, () => {
-    console.log("Server has started...");
+  console.log("Server has started...");
 });
