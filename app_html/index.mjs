e60386f73e917726614b0af42eaea084a49c1a9a
fabiohbfreitas <difreitas.fabio@gmail.com>
2024-04-24 10:43:55 -0300

    express with template engine

---
+++ app_html/index.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+import mustacheExpress from "mustache-express";
+import { resolve as resolvePath } from "node:path"
+
+const app = express();
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', resolvePath(import.meta.dirname, 'views'));
+
+app.get("/", (_, res) => {
+    return res.render("index.html", { name: "Fábio" });
+})
+app.get("/contato", (_, res) => res.render("contato.html"));
+
+app.listen(8090, () => {
+    console.log("Server has started on :8090")
+})
